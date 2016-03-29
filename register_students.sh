46c436d945822d75533d67c4fdede807fc7bd9e0
Will Starms <vilhelmen@gmail.com>
2016-03-29 17:47:21 -0500

    Minor changes, going to try python again...

--- register_students.sh
+++ register_students.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -ne 2 ]; then
-	echo "$0 <REPO_ROOT> <REGISTER>"
+	echo "$0 REPO_ROOT REGISTER_FILE"
 	exit 1
 fi
 
@@ -22,21 +22,43 @@ fi
 
 # User input probably ok, anything bad past here is mildly catostrophic
 
-REPO_ROOT=${1}
-REGISTER_FILE=${2}
-
 # strip all comments and blank lines
 REGISTER_DATA=($(sed '/^[[:blank:]]*#/d;s/#.*//;/^\s*$/d' ${REGISTER_FILE}))
-
 
 STUDENT_IDS=($(printf "%s\n" ${REGISTER_DATA[@]} | cut -d, -f2))
 
 STUDENT_REPOS=($(printf "%s\n" ${REGISTER_DATA[@]} | cut -d, -f1))
 
+if [[ ! -d workspaces ]]; then
+	echo "Workspace dir does not exist, creating..."
+	if ! mkdir workspaces; then
+		echo "Could not create directory!"
+		exit 1
+	fi
+fi
+
+if [[ ! -d logs ]]; then
+	echo "Logging dir does not exist, creating..."
+	if ! mkdir logs; then
+		echo "Could not create logging directory!"
+		exit 1
+	fi
+fi
+
+{
 for i in "${!STUDENT_IDS[@]}";do
-	echo ${STUDENT_IDS[${i}]} ${STUDENT_REPOS[${i}]}
-	git submodule add --name ${STUDENT_IDS[${i}]} ${STUDENT_REPOS[${i}]} workspaces/${STUDENT_IDS[${i}]}
+	echo "IMPORT '${STUDENT_IDS[${i}]}' @ '${STUDENT_REPOS[${i}]}'"
+	git submodule add --quiet --name ${STUDENT_IDS[${i}]} ${STUDENT_REPOS[${i}]} workspaces/${STUDENT_IDS[${i}]}
+	# --name will set the module name to just the student's id
+	# as opposed to workspaces/${ID}
+
+	# I suppose you may want --recurse in some cases if they have submodules
+	# But that's more testing than I have the time for and not in our main scope
 done
+
+# } |& tee logs/last_import.log
+# of course it's not portable because it's still 2007 apparently
+} 2>&1 | tee logs/last_import.log
 
 git add --all
 
